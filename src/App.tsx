817f67352f2af06ac795dd41b666fbe286b8fb0b
obrenoco <bsromeiro@gmail.com>
2021-10-12 03:22:45 -0300

    fix: position

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,20 @@ import { Home } from "./pages/Home/Home";
 export default function App() {
   return (
     <div className="App">
-      <div style={{ position: "fixed", margin: "0 auto" }}>
-        <h1 style={{ textAlign: "center" }}>Brasil SVG</h1>
-        <Home />
-        <a
-          href="https://github.com/obrenoco/brasil-svg"
-          target="_blank"
-          rel="noreferrer"
-          id="author"
-        >
-          <img
-            src="https://i.imgur.com/NfKqtes.png"
-            alt="Link to project on Github."
-            style={{ width: "2.4rem", height: "2.4rem" }}
-          ></img>
-        </a>
-      </div>
+      <h1 style={{ textAlign: "center" }}>Brasil SVG</h1>
+      <Home />
+      <a
+        href="https://github.com/obrenoco/brasil-svg"
+        target="_blank"
+        rel="noreferrer"
+        id="author"
+      >
+        <img
+          src="https://i.imgur.com/NfKqtes.png"
+          alt="Link to project on Github."
+          style={{ width: "2.4rem", height: "2.4rem" }}
+        ></img>
+      </a>
     </div>
   );
 }
