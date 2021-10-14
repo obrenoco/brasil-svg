acda7d038003f3744724c84dbcd175e68aa6c6e6
obrenoco <bsromeiro@gmail.com>
2021-10-13 23:02:16 -0300

    add: tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Tippy from "@tippyjs/react";
 import React from "react";
 import "./index.css";
 import { Home } from "./pages/Home/Home";
@@ -5,20 +6,23 @@ import { Home } from "./pages/Home/Home";
 export default function App() {
   return (
     <div className="App">
-      <h1 style={{ textAlign: "center" }}>Brasil SVG</h1>
+      <h1 className="text-center text-7xl w-full m-auto my-7">Brasil SVG</h1>
       <Home />
-      <a
-        href="https://github.com/obrenoco/brasil-svg"
-        target="_blank"
-        rel="noreferrer"
-        id="author"
-      >
-        <img
-          src="https://i.imgur.com/NfKqtes.png"
-          alt="Link to project on Github."
-          style={{ width: "2.4rem", height: "2.4rem" }}
-        ></img>
-      </a>
+      <Tippy content="Made with ❤️" placement="top-start">
+        <a
+          href="https://github.com/obrenoco/brasil-svg"
+          target="_blank"
+          rel="noreferrer"
+          id="author"
+          className="fixed bottom-3 right-8 no-underline text-xl"
+        >
+          <img
+            src="https://i.imgur.com/NfKqtes.png"
+            alt="Link to project on Github."
+            className="w-10 h-10"
+          ></img>
+        </a>
+      </Tippy>
     </div>
   );
 }
